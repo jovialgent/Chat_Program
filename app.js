7a936826c2f6e02ba7dd7dd26c4a1ba5911395a4
George Petersen <gfrankpetersen@gmail.com>
2012-12-20 13:42:33 -0700

    Commit with all dependencies downloaded and maintained.

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes/index')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+//All the GET Routes
+app.get('/', routes.home);
+app.get('/users', user.list);
+app.get('/success', routes.success);
+
+//All the POST Routes
+app.post('/', routes.home_post_handler);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,61 @@
+
+/*
+ * GET home page.
+ */
+
+ var _mysql   = require('mysql'),
+ HOST     = 'localhost',
+ USERNAME = 'root',
+ PASSWORD = 'root',
+ DATABASE = 'test',
+ TABLE    = 'usernames_passwords';
+
+
+
+ var mysql = _mysql.createConnection({
+ 	host: HOST,
+ 	port: 3306,
+ 	user: USERNAME,
+ 	password: PASSWORD
+
+ });
+
+
+
+
+ exports.home = function(req, res){
+ 	if(req.session.username == undefined || req.session.password == undefined || req.session.username == "NULL" || req.session.password=="NULL"){
+ 		res.render('index', {title: "Log in Please"});
+ 	}
+ 	else{
+ 		var temp = DATABASE;
+ 		var temp_user = req.session.username;
+ 		mysql.query("USE " + DATABASE);
+ 		mysql.query("SELECT password FROM " + TABLE + " WHERE username='"+ temp_user + "'", function(err, rows, fields){
+ 			if(err) res.render('index', {title:"Username/Password not found"});
+ 			else{
+ 				if(rows[0] != undefined){
+ 					temp = rows[0];
+ 					if(temp['password'] == req.session.password){
+						res.redirect('https://base.msudenver.edu/chat'); 
+ 					}				
+ 				}
+ 			}
+ 		});
+ 	}
+
+
+ };
+
+ exports.home_post_handler = function(req, res){
+ 	username = req.body.username || "NULL";
+ 	password = req.body.password || "NULL";
+ 	req.session.username = username;
+ 	req.session.password = password;
+ 	res.redirect('/');
+ };
+
+
+ exports.success = function(req, res){
+ 	res.render('successful', {title: "SUCCESS!"});
+ }
